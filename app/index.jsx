178f5d0bbdc2d3fb445b5cd9b66dd4bf9d9392ec
Haidar_cyber <alliyabdullahi@gmail.com>
2026-08-03 14:45:52 +0100

    Welcome button added

--- app/index.jsx
+++ app/index.jsx
@@ -2,49 +2,43 @@ import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import ButtonSample from "../components/button";
 
-
-
 export default function Index() {
   return (
-    <SafeAreaView style= {styles.safeAreaViewStyle} >    
-
-    
-{/* Create Button */}
-      <ButtonSample 
-      route={'/create'}
-      
-      text={'Create'}
-      backgroundColor={'green'}
+    <SafeAreaView style={styles.safeAreaViewStyle}>
+      {/* Create Button */}
+      <ButtonSample
+        route={"/create"}
+        text={"Create"}
+        backgroundColor={"green"}
       />
 
-{/* Sign In Button */}
-      <ButtonSample 
-      route={'/sign-in'}
-      text={'Sign In'}
-      backgroundColor={'blue'}
+      {/* Sign In Button */}
+      <ButtonSample
+        route={"/sign-in"}
+        text={"Sign In"}
+        backgroundColor={"blue"}
       />
 
-{/* Forgot Password Button */}
-      <ButtonSample 
-      route={'/forgot-password'}
-      text={'Forgot Password'}
-      backgroundColor={'red'}
+      {/* Forgot Password Button */}
+      <ButtonSample
+        route={"/forgot-password"}
+        text={"Forgot Password"}
+        backgroundColor={"red"}
       />
 
-
-
-
+      {/* Welcome page Button */}
+      <ButtonSample
+        route={"/welcome"}
+        text={"Welcome"}
+        backgroundColor={"orange"}
+      />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-
-    safeAreaViewStyle: {
-      gap: 20,
-      marginHorizontal: 30
-    }
-
-
-})
-
+  safeAreaViewStyle: {
+    gap: 20,
+    marginHorizontal: 30,
+  },
+});
