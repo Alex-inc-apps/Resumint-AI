f6b38ae9a21498480e298940ad56843d8caa7f21
Alex-inc-apps <alex.inc.apps@gmail.com>
2026-08-06 13:10:28 +0100

    merged

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,14 +2,19 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-   <Stack screenOptions={{headerShown: false}}>
-    {/* <Stack.Screen name= 'index'/> */}
-    <Stack.Screen name= 'create'/>
-    <Stack.Screen name= 'sign-in'/>
-    <Stack.Screen name= 'forgot-password'/>
-    <Stack.Screen name= 'welcome'/>
-
-  </Stack>
-  )
+    <Stack screenOptions={{ headerShown: false }}>
+      {/* <Stack.Screen name= 'index'/> */}
+      <Stack.Screen name="create" />
+      <Stack.Screen name="sign-in" />
+      <Stack.Screen name="forgot-password" />
+      <Stack.Screen name="welcome" />
+      <Stack.Screen name="upload-screen" />
+      <Stack.Screen name="resume-upload-applicant" />
+      <Stack.Screen name="resume-upload-recruiter" />
+      <Stack.Screen name="upload-success" />
+      <Stack.Screen name= 'create-job-listing'/>
+      <Stack.Screen name="(tabs)" />
+    </Stack>
+  );
 }
  
